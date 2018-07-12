557a08b6256a2ffd229847682671e01a1396e3bd
Eden Reich <eden.reich@gmail.com>
2018-07-13 01:21:32 +0200

    add the bussnies logic

--- application/app/Http/Controllers/CashMachineController.php
+++ application/app/Http/Controllers/CashMachineController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Interfaces\CashMachineInterface as CashMachine;
+
+use InvalidArgumentException;
+use App\Exceptions\NoteUnavailableException;
+
 class CashMachineController extends Controller
 {
     /**
@@ -12,5 +18,21 @@ class CashMachineController extends Controller
     public function index()
     {
         return view('index');
+    }
+
+    /**
+     * Withdraws money for the client.
+     *
+     * @param \Illuminate\Http\Request | $request
+     * @param \App\Classes\CashMachine | $cashMachine
+     * @return JSON
+     */
+    public function withdraw(Request $request, CashMachine $cashMachine)
+    {
+        $notes = $cashMachine->withdraw($request->input('notes'));
+
+        return response()->json([
+            'notes' => $notes
+        ]);
     }
 }
